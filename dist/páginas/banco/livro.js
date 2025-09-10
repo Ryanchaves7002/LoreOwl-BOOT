38791e4d0418afdb460201d66c16348d63caee33
RyanoBabau <ryanobabau7002@gmail.com>
2025-09-10 10:45:20 -0300

    commit8

--- dist/páginas/banco/livro.js
+++ dist/páginas/banco/livro.js
@@ -1,6 +1,6 @@
 // Função que abre a página do livro usando URL
-function openBookPage(titulo, preco, author, img) {
-    const url = `./páginas/livro.html?title=${encodeURIComponent(titulo)}&price=${encodeURIComponent(preco)}&auzthor=${encodeURIComponent(author)}&img=${encodeURIComponent(img)}`;
+function openBookPage(titulo, preco, author, img, publisher, year, description) {
+    const url = `./páginas/livro.html?title=${encodeURIComponent(titulo)}&price=${encodeURIComponent(preco)}&author=${encodeURIComponent(author)}&img=${encodeURIComponent(img)}&publisher=${encodeURIComponent(publisher)}&year=${encodeURIComponent(year)}&description=${encodeURIComponent(description)}`;
     window.location.href = url;
 }
 
@@ -9,15 +9,16 @@ const livros = document.querySelectorAll(".livro");
 livros.forEach(livro => {
     livro.addEventListener("click", () => {
         const titulo = livro.querySelector(".titulo").textContent;
-        const descricao = livro.querySelector(".descricao").textContent;
         const author = livro.querySelector(".author")?.textContent || "Autor desconhecido";
         const preco = livro.querySelector(".preco")?.textContent || "0.00";
         const img = livro.querySelector("img").getAttribute("src");
-
+        const publisher = livro.querySelector(".publisher")?.textContent || "Editora desconhecida";
+        const year = livro.querySelector(".year")?.textContent || "Ano desconhecido";
+        const description = livro.querySelector(".description")?.textContent || "Descrição não disponível";
         // salva no localStorage (opcional, caso queira usar)
-        localStorage.setItem("livroSelecionado", JSON.stringify({ titulo, descricao, author, preco, img }));
+        localStorage.setItem("livroSelecionado", JSON.stringify({ titulo, author, preco, img, publisher, year, description }));
 
         // redireciona usando a função
-        openBookPage(titulo, preco, author, img);
+        openBookPage(titulo, preco, author, img, publisher, year, description);
     });
 });
